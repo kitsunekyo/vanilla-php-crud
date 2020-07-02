38d23b6bbd670aa07d6a906ed95cbb29447cfdcd
Alex Spieslechner <alexander.spieslechner@gmail.com>
2020-07-03 00:56:58 +0200

    include time in rendered event date

--- client/index.js
+++ client/index.js
@@ -65,6 +65,8 @@ Vue.component("events-list", {
                 year: "numeric",
                 month: "long",
                 day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit"
             }),
     },
     template: `
